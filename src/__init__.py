ceccf9539bdb968ec804f41382022dc373eac9c4
Subhobhai <sarkarsubhadip604@gmail.com>
2026-01-05 14:44:56 +0530

    Fix syntax error in src/__init__.py - convert to proper Python docstring

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
-# SUB-AGI Python package
+"""SUB-AGI Python package
 
 This file marks `src` as a Python package root when using certain tooling.
 All core implementation lives in subpackages like `mind_kernel`.
+"""
+
+# Package version
+__version__ = "0.1.0"
